46d623e722ba7ffc8eb7f01afbfc81228fa5f475
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2025-06-26 01:17:35 +0600

    assignment operator

---
+++ basic_python/arithemetic_operaators.py
@@ -0,0 +1,40 @@
+# +,-,*,/,%,**,//
+
+a=10
+b=20
+#addition
+
+add = a+b
+print(add)
+
+#sub
+
+sub = a-b
+print(sub)
+
+#mul
+
+mul = a*b
+print(mul)
+
+#div
+
+div = b/a
+print(div)
+
+#mod
+
+mod = b%a
+print(mod)
+print(a%3)
+
+#5*5
+print(5**2)
+
+#5*5*5
+print(5**3)
+
+#floor division
+print(10/3) #as usual
+
+print(20//3) #floor div
---
+++ basic_python/assignment_operator.py
@@ -0,0 +1,22 @@
+# =, +=, -=
+
+#equal(=) operator
+a = 5
+print(a)
+
+x = a+6
+print(x)
+
+#increment
+a += 2
+print(a)
+
+x+=2
+print(x)
+
+#decrement
+a -= 2
+print(a)
+
+x-=2
+print(x)
