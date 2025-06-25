73f6ff2a6d4a5b7345cd39b7308b88549a862374
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2025-06-25 22:45:17 +0600

    basic of var

---
+++ basic_python/var.py
@@ -0,0 +1,30 @@
+#Create variable
+a = 10
+b = 20
+
+print(a,b)
+
+#Memory address of variable
+
+print(id(a), id(b))
+
+#if two var. with same value then memory add.(it will show same memory location)
+
+a1 = 10
+b1 = 10
+
+print (id(a1), id(b1))
+
+#check type of variable
+
+name = 'Zahid'
+
+print (type(a), type(b))
+print (type(a1), type(b1))
+print (type(name))
+
+#length of string
+print (len(name))
+
+
+
